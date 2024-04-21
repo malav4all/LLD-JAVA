34cf6feaae8cb6d1c4b451ec9a676ef8107651de
Malav Naagar <malavnagar90@gmail.com>
2024-04-21 14:02:02 +0530

    basic class make in java

---
+++ LLD-Course/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Student Malav = new Student();
+        Malav.name = "PRO DEV";
+        Malav.address = "Rohini";
+        Malav.state = "ACTIVE";
+        Malav.pauseCourse();
+        System.out.println("Hello, World!");
+    }
+}
---
+++ LLD-Course/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name;
+    String address;
+    String email;
+    String batch;
+    double psp;
+    String state;
+
+    void changeBatch(String newBatch) {
+        this.batch = newBatch;
+    }
+
+    void pauseCourse() {
+        this.state = "PAUSED";
+    }
+}
